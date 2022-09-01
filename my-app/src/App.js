b982a1a9a2d43adc4c306d70d4b5f7b71fcc622a
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-09-01 01:06:39 +0100

    components

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header/Header';
+import "./assets/js/script";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
