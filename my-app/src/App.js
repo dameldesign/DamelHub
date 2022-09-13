082dd04d066965ec255d5969f4ff5739afa88369
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-09-13 15:00:27 +0100

    progress

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import "./assets/js/script";
 import Services from './components/Services/Services';
+import About from './components/About/About';
+import Qualities from './components/Qualities/Qualities'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Services />
+      <About />
+      <Qualities />
     </div>
   );
 }
